893748a1a7a88adf0bf8eccf07814d5e98897385
Run4gate <westernisator@gmail.com>
2024-12-12 18:49:24 +0300

    feat: add commitlint to enforce type declaration of a commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,12 @@
-const Configuration = {
-  extends: ["@commitlint/config-conventional"],
-  parserPreset: "conventional-changelog-atom",
-  formatter: "@commitlint/format",
-  rules: {
-    "header-pattern": [
-      2, // Error level (reject commits that don't match)
-      "always",
-      "^([A-Za-z0-9]+)-([0-9]+)\\s", // Regex for ProjectName-TaskNumber
-    ],
-    "type-enum": [2, "always", ["feat", "fix", "chore"]],
-  },
-};
-
 export default Configuration;
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "header-pattern": [
+          2,
+          "always",
+          "^([A-Za-z0-9]+)-([0-9]+)\\s",
+        ],
+        "type-enum": [2, "always", ["feat", "fix", "chore"]],
+      },
+  };
