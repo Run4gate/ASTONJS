c0a5cdc460d40ba8c0249ce2767f6af79d95a5c8
Run4gate <westernisator@gmail.com>
2024-12-16 16:52:47 +0300

    chore: use prettier

--- Lesson 2 HTTP/compare.js
+++ Lesson 2 HTTP/compare.js
@@ -1,12 +1,12 @@
-import isEqual from 'lodash/isEqual.js'
+import isEqual from "lodash/isEqual.js";
 let compare = (value, other) => {
-    return isEqual(value, other)
-} // simply using lodash's isEqual method, i suspect it wasnt not the desired solution so providing my own compare2 function next 
+  return isEqual(value, other);
+}; // simply using lodash's isEqual method, i suspect it wasnt not the desired solution so providing my own compare2 function next
 let compare2 = (value, other) => {
-        if (typeof value !== 'object') return value === other;
-        let keys = Object.keys(value)
-        for (let key of keys) {
-            if (!compare2(value[key], other[key])) return false;
-        }
-        return true;
-}
+  if (typeof value !== "object") return value === other;
+  let keys = Object.keys(value);
+  for (let key of keys) {
+    if (!compare2(value[key], other[key])) return false;
+  }
+  return true;
+};
--- Lesson 2 HTTP/length.js
+++ Lesson 2 HTTP/length.js
@@ -1,23 +1,23 @@
 const getLength = (any) => {
-    let type = typeof any
-    let length = 0
-    switch (type) {
-        case 'object':
-            if (Array.isArray(any)) {
-                length = any.length
-            } else if (any.length) {
-                length = any.length
-            } else if (any instanceof Map || any instanceof Set) {
-                length = any.size
-            } 
-            break
-        case 'string':
-        case 'function':
-            length = any.length
-            break
-        default:
-            break
-    }
-    console.log(length)
-    return ''
-}; 
+  let type = typeof any;
+  let length = 0;
+  switch (type) {
+    case "object":
+      if (Array.isArray(any)) {
+        length = any.length;
+      } else if (any.length) {
+        length = any.length;
+      } else if (any instanceof Map || any instanceof Set) {
+        length = any.size;
+      }
+      break;
+    case "string":
+    case "function":
+      length = any.length;
+      break;
+    default:
+      break;
+  }
+  console.log(length);
+  return "";
+};
