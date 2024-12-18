d06e0a6f22845299dfff239f8145097d1e6c77a3
Run4gate <westernisator@gmail.com>
2024-12-18 17:11:08 +0300

    fix: add checks for previously unchecked types of input

--- Lesson 2 HTTP/length.js
+++ Lesson 2 HTTP/length.js
@@ -3,7 +3,7 @@ const getLength = (any) => {
   switch (type) {
     case "object":
       if (Array.isArray(any)) {
-        console.log(any.length)
+        console.log(any.length);
       } else if (any !== null && any.length) {
         console.log(any.length);
       } else if (any instanceof Map || any instanceof Set) {
@@ -12,11 +12,19 @@ const getLength = (any) => {
       break;
     case "string":
     case "function":
-      console.log(any.length)
+      console.log(any.length);
       break;
+    case "number":
+    case "bigint":
+      console.log(("" + any).length);
+      break;
+    case "symbol":
+      console.log(any.description.length);
+      break;
+    case "boolean":
     default:
+      console.log(0);
       break;
   }
-  
   return "";
 };
