d8cb30269482d2d5ee9445bd4ad05f55b281dd07
Run4gate <westernisator@gmail.com>
2024-12-23 01:11:51 +0300

    feat: solved lesson 4's advanced task

--- Lesson 4 Objects/basicTask.js
+++ Lesson 4 Objects/basicTask.js
@@ -1,7 +1,7 @@
 // First task
 
 // Arrays in JS are considered to be "abnormal" among their brethren in other languages
-// due to the fact that essentially they are as flexible as JS itself 
+// due to the fact that essentially they are as flexible as JS itself
 // by being able to contain multiple different types of data,
 // are instances of objects (using key:value pair under the hood),
 // while being isntances of their own 'array' prototype at the same time,
@@ -10,11 +10,11 @@
 
 // Second task
 
-const obj = { item: 'some value' }
+const obj = { item: "some value" };
 function logger() {
   console.log(`I output only external context: ${this.item}`);
 }
 
 logger = logger.bind(obj)
-logger.apply(obj)
-logger.call(obj)
+// logger.apply(obj)
+// logger.call(obj)
